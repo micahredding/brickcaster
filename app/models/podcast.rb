85749fca3d0971e0a2c32091f80af8ffd10049ea
Micah Redding <micahtredding@gmail.com>
2013-05-08 23:39:35 -0500

    simplifying podcast sidebar links functionality

--- app/models/podcast.rb
+++ app/models/podcast.rb
@@ -34,40 +34,16 @@ class Podcast < ActiveRecord::Base
 
   def subscribe_links
     subscribe_links_hash = {}
-
     subscribe_links_hash['Subscribe in iTunes'] = subscribe_itunes_link
     subscribe_links_hash['Subscribe in RSS'] = rss_url
     # subscribe_links_hash['Subscribe in Feedburner'] = subscribe_feedburner_link
 
-    subscribe_links_hash
-  end
-
-  def custom_links
-    links_hash_list = {}
-
-    if links
-      links.lines.map{ |h| 
-        h1,h2 = h.split('|')
-        if h1 and h2
-          links_hash_list[h1] = h2.chomp!
-        end
-      }
-    end
-
-    links_hash_list
-  end
-
-  def links_hash
-    links_hash_list = subscribe_links.merge custom_links 
-    links_hash_list
-  end
-
-  def links_html
     string = ""
-    links_hash.each do 
+    subscribe_links_hash.each do 
       |key, value|
       string += "<a href='#{value}'>#{key}</a>"
     end
+
     string
   end
 
