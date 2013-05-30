ef0fd4e05c177c5ecbd33af400d1b7c2ac48ccfc
Micah Redding <micahtredding@gmail.com>
2013-05-29 23:15:52 -0500

    Simplify

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -7,6 +7,7 @@ class Episode < ActiveRecord::Base
 
   def load_media_properties
     # Load a file
+    unless media_local_url return
     TagLib::FileRef.open(media_local_url) do |fileref|
       unless fileref.null?
         tag = fileref.tag
