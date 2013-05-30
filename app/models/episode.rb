ab4be983891f4414e495e5cc59818b6793d2c169
Micah Redding <micahtredding@gmail.com>
2013-05-29 22:44:06 -0500

    Altering episode file reading

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -3,7 +3,7 @@ class Episode < ActiveRecord::Base
   attr_accessible :episode_number, :title, :author, :body, :media_url, :podcast_id, :publish_date
   attr_accessor :media_length, :media_title, :media_artist, :media_album, :media_year, :media_track, :media_genre, :media_comment
 
-  after_initialize :load_media_properties
+  after_save :load_media_properties
 
   def load_media_properties
     # Load a file
