f8368327f8b377aa024302b703dc8ed448fb8f0f
Micah Redding <micahtredding@gmail.com>
2013-06-26 19:20:03 -0500

    Changing number fields to string to work with postgres

--- db/migrate/20130625015105_add_media_fields_to_episodes.rb
+++ db/migrate/20130625015105_add_media_fields_to_episodes.rb
@@ -1,10 +1,10 @@
 class AddMediaFieldsToEpisodes < ActiveRecord::Migration
   def change
-    add_column :episodes, :media_length, :number
+    add_column :episodes, :media_length, :string
     add_column :episodes, :media_title, :string
     add_column :episodes, :media_artist, :string
     add_column :episodes, :media_album, :string
-    add_column :episodes, :media_year, :number
+    add_column :episodes, :media_year, :string
     add_column :episodes, :media_track, :string
   end
 end
