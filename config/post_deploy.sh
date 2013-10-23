a2228712960872fa55e0518e6bd4f47dc5283e85
Micah Redding <micahtredding@gmail.com>
2013-10-23 16:19:29 -0500

    Deploy script and Compile

--- config/post_deploy.sh
+++ config/post_deploy.sh
@@ -1,3 +1,4 @@
+gem install bundler
 RAILS_ENV=production bundle exec rake assets:precompile
 /usr/local/rvm/gems/ruby-1.9.3-p429/bin/passenger stop -p 80
 /usr/local/rvm/gems/ruby-1.9.3-p429/bin/passenger start -p 80 -d -e production
