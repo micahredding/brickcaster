01869815c9585a7f2138a9634719aae9eb1dac65
Micah Redding <micahtredding@gmail.com>
2013-10-23 16:22:52 -0500

    Adding Rake ENV

--- config/post_deploy.sh
+++ config/post_deploy.sh
@@ -1,4 +1,3 @@
-gem install bundler
 RAILS_ENV=production bundle exec rake assets:precompile
 /usr/local/rvm/gems/ruby-1.9.3-p429/bin/passenger stop -p 80
 /usr/local/rvm/gems/ruby-1.9.3-p429/bin/passenger start -p 80 -d -e production
