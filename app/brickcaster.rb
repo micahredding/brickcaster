7bbbd971688159c8dfa0e0fb78d9af1487256895
Micah Redding <micahtredding@gmail.com>
2014-08-13 21:29:55 -0500

    separating out helpers file

--- app/brickcaster.rb
+++ app/brickcaster.rb
@@ -4,35 +4,6 @@ require 'builder'
 require 'yaml'
 require 'redcarpet'
 
-module BrickcasterHelpers
-  def markdown
-  	@markdown ||= Redcarpet::Markdown.new(
-      Redcarpet::Render::HTML.new(:link_attributes => Hash["target" => "_blank"]),
-      :hard_wrap => true,
-      :autolink => true,
-      :space_after_headers => true
-    )
-  end
-
-  def format_length(length)
-    if length.nil?
-      return "00:00:00"
-    end
-    length = Time.at(length.to_i).utc.strftime("%H:%M:%S")
-  end
-
-  def format_date(date)
-    return 0 if date.nil?
-    DateTime.parse(date).rfc822
-  end
-
-  def format_date_human(date)
-    return 0 if date.nil?
-    Date.parse(date).strftime("%Y.%m.%d")
-  end
-
-end
-
 helpers BrickcasterHelpers
 
 get '/:podcast_id/:episode_number' do
