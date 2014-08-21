cfa6fd279cc9f27aab484244b870d9e43839a6f7
Micah Redding <micahtredding@gmail.com>
2014-08-20 20:15:56 -0500

    Removing semicolons and redirects. Let them eat errors.

--- app/brickcaster.rb
+++ app/brickcaster.rb
@@ -3,32 +3,30 @@ require 'json'
 require 'builder'
 require 'yaml'
 require 'redcarpet'
+require 'require_all'
+require 'sinatra/export'
+require_all 'app'
 
 helpers BrickcasterHelpers
 
 get '/:podcast_id/:episode_number' do
   @podcast = Podcast.get(params[:podcast_id])
-  redirect '/' if @podcast.nil?
 	@episode = Episode.get(params[:podcast_id], params[:episode_number])
-  redirect '/' if @episode.nil?
 	erb :episode
 end
 
 get '/:podcast_id.json' do
-	@podcast = Podcast.get(params[:podcast_id]);
-  redirect '/' if @podcast.nil?
+	@podcast = Podcast.get(params[:podcast_id])
 	@podcast.json
 end
 
 get '/:podcast_id.rss' do
-	@podcast = Podcast.get(params[:podcast_id]);
-  redirect '/' if @podcast.nil?
+	@podcast = Podcast.get(params[:podcast_id])
 	builder :podcast_rss
 end
 
 get '/:podcast_id' do
-	@podcast = Podcast.get(params[:podcast_id]);
-  redirect '/' if @podcast.nil?
+	@podcast = Podcast.get(params[:podcast_id])
 	erb :podcast
 end
 
