70d91935f11cb5afb98144d1da9e81838fe9fe57
Micah Redding <micahtredding@gmail.com>
2014-08-20 20:34:40 -0500

    removing json route and adding podcast redirects

--- app/brickcaster.rb
+++ app/brickcaster.rb
@@ -12,22 +12,20 @@ helpers BrickcasterHelpers
 PODCASTS = ["abstraction", "singularity", "christianity"]
 
 get '/:podcast_id/:episode_number' do
+  redirect '/' if !PODCASTS.include? params[:podcast_id]
   @podcast = Podcast.get(params[:podcast_id])
 	@episode = Episode.get(params[:podcast_id], params[:episode_number])
 	erb :episode
 end
 
-get '/:podcast_id.json' do
-	@podcast = Podcast.get(params[:podcast_id])
-	@podcast.json
-end
-
 get '/:podcast_id.rss' do
+  redirect '/' if !PODCASTS.include? params[:podcast_id]
 	@podcast = Podcast.get(params[:podcast_id])
 	builder :podcast_rss
 end
 
 get '/:podcast_id' do
+  redirect '/' if !PODCASTS.include? params[:podcast_id]
 	@podcast = Podcast.get(params[:podcast_id])
 	erb :podcast
 end
